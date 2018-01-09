64758d295421ab754188b38b8e3ee517d211eb7b
SaltySheldon <32780787+SaltySheldon@users.noreply.github.com>
2018-01-09 11:57:30 -0700

    I think i fixed my git sync issue + added a favicon

--- index.php
+++ index.php
@@ -8,47 +8,18 @@
 		<title> 
 			Augustana Student Academic Conference Website
 		</title>
-<<<<<<< HEAD
-		<link rel="stylesheet" href="resources\css\app.css">
-=======
 		<link rel="stylesheet" href="resources/css/foundation.css">
     	<link rel="stylesheet" href="resources/css/app.css">
->>>>>>> 67584355420d6bbb885ae9ed678c45868de13258
+    	<link rel="shortcut icon" href="resources\Augfavicon.ico" type="image/x-icon">
 	</head>
 
 	<body>
-		<div class="row">
-			<div class="large-12 columns">
-				<h1>
-					Welcome to the Augustana Student Academic Conference!
-				</h1>
-			</div>
 		<div class="header">
 			<h1>
 				<a href="index.php">Augustana Student Academic Conference</a>
 			</h1>
 		</div>
 
-		<div class ="row">
-			<h2>
-				This is Sheldon's local website right <i>now</i>!
-			</h2>
-			<h2>
-				<p>
-					This website will help me:
-					<ul>
-						<li>
-							learn new skills
-						</li>
-						<li>
-							build my portfolio
-						</li>
-						<li>
-							achieve a great mark
-						</li>
-					</ul>
-				</p>
-			</h2>
 		<div id="loginField" class="callout secondary">
 			<h3 id="loginDisplay">
 				Login
@@ -61,16 +32,7 @@
 					<a href="registration.php">Don't have an account yet?</a>
 				</ul>
 			</form>
->>>>>>> 67584355420d6bbb885ae9ed678c45868de13258
 		</div>
-	
-		<div class ="row">
-<form action="process.php" method="post">
-	<input name = "name" type="text">
-	<input name = "lName" type = "text">
-	<input type = "submit">
-</form>
-</div>
 
 		<div class="footer">
 			Designed January 7th, 2018<br>
@@ -80,10 +42,6 @@
 		<script src="js/vendor/foundation.js"></script>
     	<script src="js/app.js"></script>
 
-<address>Made 07 January 2018<br>
-  by Alex Ho, Connor Maschke, Sheldon Grundberg.</address>
 
 	</body>
-
-
 </html>
