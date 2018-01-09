8bd8f1dea4fcecc4f77054fa2722f6c65fc0d7d3
AlexRHo <32780777+AlexRHo@users.noreply.github.com>
2018-01-09 14:51:50 -0700

    changed the password to not show up

--- index.php
+++ index.php
@@ -11,22 +11,19 @@
 		<link rel="stylesheet" href="resources/css/foundation.css">
     	<link rel="stylesheet" href="resources/css/app.css">
 	</head>
-
 	<body>
-
 		<div class="header">
 			<h1>
 				<a href="index.php">Augustana Student Academic Conference</a>
 			</h1>
 		</div>
-
 		<div id="loginField" class="callout secondary">
 			<h3 id="loginDisplay">
 				Login
 			</h3>
 			<form id="loginForm" action="userpage.php" method="GET">
 				<input name="username" type="text" placeholder="Username" required>
-				<input name="password" type = "text" placeholder="Password" required>
+				<input name="password" type = "password" placeholder="Password" required>
 				<input value="Submit" type="submit">
 				<ul>
 					<a href="registration.php">Don't have an account yet? Register Here</a>
@@ -41,7 +38,5 @@
 
 		<script src="js/vendor/foundation.js"></script>
     	<script src="js/app.js"></script>
-
-
 	</body>
 </html>
