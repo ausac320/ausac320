6ceb815880a6b8f14ba927003ed16417430eca13
Connor <cmaschke@ualberta.ca>
2018-01-12 11:24:22 -0700

    Moved index page css to it's own file and added style sheet ref and cleaned up code a bit

--- index.php
+++ index.php
@@ -6,40 +6,42 @@
 		<meta http-equiv="x-ua-compatible" content="ie=edge">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<meta name="google-signin-client_id" content="514360117392-qkk1ff10ud9svjk0mm08piuvmncregp6.apps.googleusercontent.com">
-		
     	<title> 
 			Augustana Student Academic Conference Website
 		</title>
 		<link rel="stylesheet" href="resources/css/foundation.css">
     	<link rel="stylesheet" href="resources/css/app.css">
+    	<link rel="stylesheet" href="resources/css/index.css">
     	<link rel="shortcut icon" href="resources\images\Augfavicon.ico" type="image/x-icon">
 	</head>
 	
 	<body>
+		
 		<div class="header">
 			<h1>
 				<a href="index.php">Augustana Student Academic Conference</a>
 			</h1>
 		</div>
+
 		<div id="loginField" class="callout secondary">
 			<h3 id="loginDisplay">Login</h3>
-      
 			<form id="loginForm" action="userpage.php" method="POST">
 				<div class="g-signin2" data-onsuccess="onSignIn" required>
-			</div>
+				</div>
 				<input name="username" type="text" placeholder="CCID" required>
 				<input name="password" type = "password" placeholder="Password" required>
-
 				<input value="Submit" type="submit">
 				<ul>
 					<a href="registration.php">Don't have an account yet? Register Here</a>
 				</ul>
 			</form>
 		</div>
+
 		<div class="footer">
 			Designed January 7th, 2018<br>
   			by Sheldon Grundberg, Alex Ho, and Connor Maschke.
 		</div>
+
 		<script src="js/vendor/foundation.js"></script>
 		<script src="https://apis.google.com/js/platform.js" async defer></script>
     	<script src="js/app.js"></script>
