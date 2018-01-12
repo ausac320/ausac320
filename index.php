4b4833984a5a6c58a85528cdc9a2efda5c5fbe82
Connor <cmaschke@ualberta.ca>
2018-01-12 11:10:33 -0700

    Revert "Moved index page css to it's own file and added style sheet ref"
    This reverts commit a06b98c1403028344608b58c6189b456bd165485.

--- index.php
+++ index.php
@@ -6,12 +6,12 @@
 		<meta http-equiv="x-ua-compatible" content="ie=edge">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<meta name="google-signin-client_id" content="514360117392-qkk1ff10ud9svjk0mm08piuvmncregp6.apps.googleusercontent.com">
+		
     	<title> 
 			Augustana Student Academic Conference Website
 		</title>
 		<link rel="stylesheet" href="resources/css/foundation.css">
     	<link rel="stylesheet" href="resources/css/app.css">
-    	<link rel="stylesheet" href="resource/css/index.css">
     	<link rel="shortcut icon" href="resources\images\Augfavicon.ico" type="image/x-icon">
 	</head>
 	
@@ -40,7 +40,6 @@
 			Designed January 7th, 2018<br>
   			by Sheldon Grundberg, Alex Ho, and Connor Maschke.
 		</div>
-		
 		<script src="js/vendor/foundation.js"></script>
 		<script src="https://apis.google.com/js/platform.js" async defer></script>
     	<script src="js/app.js"></script>
