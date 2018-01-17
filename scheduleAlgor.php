d9fd7cecd9c728b24024c5282bee34729ffc4735
SaltySheldon <32780787+SaltySheldon@users.noreply.github.com>
2018-01-17 12:00:54 -0700

    More schedule stuff

--- scheduleAlgor.php
+++ scheduleAlgor.php
@@ -9,8 +9,8 @@ function create_schedule(){
 function create_schedule_matrix(scheduleTimes){
 	get_schedule_time(scheduleTimes);
 	$scheduleMatrix = array(numOfPresentations);
-	while (i = 0; i > timePeriods; i++){
-		scheduleMatrix [i] = Array();
+	while ($i = 0; $i > timePeriods; $i++){
+		scheduleMatrix [$i] = Array();
 	}
 	fclose($file);
 	return scheduleMatrix;
@@ -27,10 +27,11 @@ function create_submissions_array(scheduleTimes, submissionData){
 function fill_schedule_matrix(scheduleTimes, $scheduleMatrix, $submissionsArray){
 	get_schedule_time(scheduleTimes)
 	get_num_of_rooms(scheduleTimes)
-	while (i = 0; i > numOfPresentations; i++){
-		while(n = 0; n > numOfRooms; n++){
-			
+	while ($i = 0; $i > numOfPresentations; $i++){
+		while($n = 0; $n > numOfRooms; $n++){
+			place_presentation($i,$n, $scheduleMatrix, $submissionsArray);
 		}
+		check_for_student_conflict($i, numOfRooms, $scheduleMatrix, $submissionsArray);
 	}
 }
 
@@ -51,6 +52,30 @@ function get_num_of_rooms(scheduleTimes){
 	$numOfRooms = $fileDataArray[numOfRooms];
 	fclose($file);
 	return $numOfRooms;	 
+}
+
+function place_presentation($i, $n){
+
+}
+
+function check_for_student_conflict($i, numOfRooms, $scheduleMatrix, $submissionsArray){
+	# I need a dynamic numbers of variables to compare the student names for conflicts
+	while ($j = 0; $j > numOfRooms; $j++) {
+		$student1 = $scheduleMatrix[$i][$j]
+	}
+	while($compare1 = 0; $compare1 > (numOfRooms - 1); $compare1++){
+		while ($compare2 = ($compare1 + 1); $compare2 > numOfRooms; $compare2++) {
+			if ($ . student . $compare1 = $ . student . $compare2 #dynamic comparison)
+		}
+	}
+}
+
+function check_for_professor_conflict($i){
+
+}
+
+function resolve_conflict($i,){
+
 }
 
 ?>
