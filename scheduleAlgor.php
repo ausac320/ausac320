3e7f8e4b50890b26adf0f841ee5c14281bca1a84
SaltySheldon <32780787+SaltySheldon@users.noreply.github.com>
2018-01-17 21:43:01 -0700

    Fixed a small issue

--- scheduleAlgor.php
+++ scheduleAlgor.php
@@ -1,8 +1,6 @@
 <?php
 
 function compile_schedule(){
-	create_schedule_matrix(scheduleTimes);
-	create_submissions_array(submissionData);
 	populate_schedule_matrix($scheduleMatrix, $submissionsArray);
 }
 
@@ -50,7 +48,7 @@ function schedule_presentation($submissionsArray, $scheduleMatrix, $roomNumber, 
 
 function check_for_student_conflict($scheduleMatrix, $presEndTime, $roomNumber, $numOfRooms, $presLength){
 	$studentConflict = false;
-	$studentName = 2; #This is because the student name is stored constantly in the second element 
+	$studentName = 0; #This is because the student name is stored constantly in the second element 
 	while ($compareRoom = 4; $compareRoom > ($numOfRooms + 4); $compareRoom++) { #since our booked rooms start at 4
 		if($scheduleMatrix[$roomNumber][$presEndTime][$studentName] == $scheduleMatrix[$compareRoom][$presEndTime][$studentName]
 		and $roomNumber != $compareRoom){ #If the student is the same in the two presentations and they arent the same one
@@ -67,7 +65,7 @@ function check_for_student_conflict($scheduleMatrix, $presEndTime, $roomNumber, 
 
 function check_for_prof_conflict($scheduleMatrix, $presEndTime, $roomNumber, $numOfRooms, $presLength){
 	$profConflict = false;
-	$profName = 1; #This is because like our student name, the professor name is constantly stored in the first element
+	$profName = 5; #This is because like our student name, the professor name is constantly stored in the first element
 	while ($compareRoom = 4; $compareRoom > ($numOfRooms + 4); $compareRoom++) {
 		if($scheduleMatrix[$roomNumber][$presEndTime][$profName] == $scheduleMatrix[$compareRoom][$presEndTime][$profName]
 		and $roomNumber != $compareRoom){ #If the professor is the same in the two presentations and they arent the same one
