85de591973e3392aa868c2fdba37ffaa9f418943
Connor Maschke <cmaschke@ualberta.ca>
2018-01-19 22:38:52 -0700

    Finished editable contact documentation

--- resources/js/editableContactTable.js
+++ resources/js/editableContactTable.js
@@ -1,28 +1,65 @@
-//File Head
+/**
+ * editableContactTable.js
+ *
+ * This file contains all the functionality to be able to grab the previously saved
+ * text for the contact display field and then create the field and populate it.
+ * It also has the functionality behind the edit button and the calls to the php function
+ * for saving.
+ *
+ * Methods:
+ * addContactEditButtonToHomeAdmin() - add edit button for admin
+ * grabContactTextData() - grab data for text field
+ * createContactInfo() - create contact text field
+ * editContact() - functionality behind edit button
+ * sendStringToPHP() - sends text field to php function to save
+ *
+ * Bugs:
+ * Editable text field produces errors when trying to remove the \n at the end of a line or 
+ * if you add a new line to the text field.
+ *
+ * Status:
+ * Editable text for deadline is setup and outputs the appropriate file but causes 
+ * issues using said file to reloop through, so does not display changes currently. 
+ */
+
+// Static method that when a field is contenteditable it will constantly
+// save the innerHTML to whatever the new innerHTML is on certain keystrokes
+// such as keyup, paste, input, etc.
 
 $('body').on('focus', '[contenteditable]', function() {
     var $this = $(this);
-    $this.data('before', $this.html());
+    $this.data('before', $this.html());//old data
     return $this;
 }).on('blur keyup paste input', '[contenteditable]', function() {
     var $this = $(this);
-    if ($this.data('before') !== $this.html()) {
+    if ($this.data('before') !== $this.html()) {//did innerhtml data change?
         $this.data('before', $this.html());
-        $this.trigger('change');
+        $this.trigger('change');//if so then change
     }
     return $this;
 });
 
-//To limit clutter, only add edit button to home
+/**
+ * addContactEditButtonToHomeAdmin()
+ * 
+ * Creates the edit button only when the account is a user.
+ * It will also call the grabContactTextData method to start
+ * grabbing data for contact display. Only add edit button
+ * to home page to eleminate clutter.
+ */
+
 function addContactEditButtonToHomeAdmin(){
 	var contactRow = document.createElement('div');
 	contactRow.className = "row";
+
+	//Title
 	var contactTitle = document.createElement('h3');
 	contactTitle.className = "small-10 columns";
 	contactTitle.innerHTML = "Contact Information";
 	contactTitle.style.textDecoration = "underline";
 	contactRow.appendChild(contactTitle);
 
+	//Button
 	var editContact = document.createElement('button');
 	editContact.className = "button round warning small-2 columns";
 	editContact.id = "contactEditButton";
@@ -33,6 +70,12 @@ function addContactEditButtonToHomeAdmin(){
 	grabContactTextData();
 }
 
+/**
+ * grabContactTextData()
+ * 
+ * Grabs the text data from a .txt file and sends it to the create function.
+ */
+
 function grabContactTextData(){
 	$.ajax({
   	url: 'resources/data/contact.txt',
@@ -40,9 +83,19 @@ function grabContactTextData(){
 	}).done(createContactInfo);
 }
 
+/**
+ * createContactInfo()
+ * 
+ * Creates the html that actually displays the text.
+ * It creates as well as formats all of the <P> section for the text
+ * and adds in html linebreaks for all the .txt line feeds.
+ *
+ * @params {text} - data - the previously saved text for important dates
+ */
+
 function createContactInfo(data){
-	var allRows = data.split("\n");
-	var contactString = "";
+	var allRows = data.split("\n");//create array of rows
+	var contactString = "";//final string to display
 
 	var paragraph = document.createElement('p');
 	paragraph.id = "contactArea";
@@ -52,9 +105,19 @@ function createContactInfo(data){
 	}
 
 	paragraph.innerHTML = contactString;
+
+	//There is only 1 element in class 'contactInfo' so we know index is 0
 	var location = document.getElementsByClassName('contactInfo');
 	location[0].appendChild(paragraph);	
 }
+
+/**
+ * editContact()
+ * 
+ * Functionality of edit button.
+ * Creates new appearance for edit button as well as new functionality for
+ * it through css and html manipulation.
+ */
 
 function editContact(){
 	var buttonID = document.getElementById('contactEditButton');
@@ -67,6 +130,12 @@ function editContact(){
 	contactEdit.style.backgroundColor = "#1779ba";
 }
 
+/**
+ * sendStringToPHP()
+ * 
+ * Grabs txt in contact display and sends it to php function.
+ */
+
 function sendStringToPHP(){
 	transferString = document.getElementById('contactArea').innerHTML;
 
@@ -77,5 +146,5 @@ function sendStringToPHP(){
 		dataType: "json"
 	});
 
-	window.location.href=window.location.href;
+	window.location.href=window.location.href;//reload page once php request is done
 }
